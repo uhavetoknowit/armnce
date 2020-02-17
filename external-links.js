0a307d6dbb9fe2f5e4b23853fb291e533467aea4
uhavetoknowit <61016014+uhavetoknowit@users.noreply.github.com>
2020-02-17 13:21:38 +0200

    Add files via upload

---
+++ external-links.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementsByClassName('footer__wrapper_links')[0].setAttribute('href', 'https://asianmelodies.com/disclaimers');
+  document.getElementsByClassName('footer__wrapper_links')[1].setAttribute('href', 'https://asianmelodies.com/terms-of-use');
+  document.getElementsByClassName('footer__wrapper_links')[2].setAttribute('href', 'https://asianmelodies.com/privacy-policy');
+  document.getElementsByClassName('footer__wrapper_links')[3].setAttribute('href', 'https://asianmelodies.com/cookie-policy');
+  document.getElementsByClassName('footer__wrapper_links')[4].setAttribute('href', 'https://asianmelodies.com/report-abuse');
+}
+                          )
